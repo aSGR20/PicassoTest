3573e84140b4f18106aec8a3486a5c96189d9ae9
aSGR20 <a_sebastian.gonzalez.rios@iespablopicasso.es>
2021-02-03 21:17:52 +0100

    Agregar archivos de proyecto.

---
+++ PicassoTest/MainApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PicassoTest
+{
+    public class MainApp
+    {
+        public MainApp()
+        {
+        }
+
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
